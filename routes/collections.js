f9a15675b35156a9391c38b7191c067157d23800
Theodore Roley <tvroley@uw.edu>
2024-05-16 23:33:25 -0700

    get collection by id function

--- routes/collections.js
+++ routes/collections.js
@@ -3,6 +3,17 @@ const router = Router();
 
 const collectionDAO = require('../daos/cardCollection');
 
+router.get("/:id", async (req, res, next) => {
+    const collectionId = req.params.id;
+    
+    try {
+        const collection = await collectionDAO.getCardCollection(collectionId);
+        res.json(collection);
+    } catch(err) {
+        next(err);
+    }
+});
+
 router.post("/", async (req, res, next) => {
     if(req.body) {
         try {
@@ -15,5 +26,21 @@ router.post("/", async (req, res, next) => {
         res.sendStatus(400);
     }
 });
+
+/*router.post("/:id", async (req, res, next) => {
+    const collectionId = req.params.id;
+    const cardId = req.body.cardId;
+
+    if(cardId && collectionId) {
+        try {
+            const collection = await collectionDAO.createCardCollection(req.body);
+            res.json(collection);
+        } catch(err) {
+            next(err);
+        }
+    } else {
+        res.sendStatus(400);
+    }
+});*/
 
 module.exports = router;
