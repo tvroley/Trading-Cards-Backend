6ebab6148a705e261bee9fb83aa1e9842a6ff541
Theodore Roley <tvroley@uw.edu>
2024-05-17 14:16:18 -0700

    get card by id and put update card

--- daos/tradingCard.js
+++ daos/tradingCard.js
@@ -29,16 +29,10 @@ module.exports.getCardByCert = async (company, cert) => {
     return await Card.findOne({gradingCompany: company, certificationNumber: cert});
 }
 
-module.exports.updateItem = async (cardId, cardObj) => {
+module.exports.updateCard = async (cardId, cardObj) => {
     if (!mongoose.Types.ObjectId.isValid(cardId)) {
         throw new errors.InvalidMongooseId("Invalid trading card ID");
     }
-    const myObj = {_id: cardId, year: cardObj.year, brand: cardObj.brand, cardNumber: cardObj.cardNumber, 
-        cardSet: cardObj.cardSet, subject: cardObj.subject, variety: cardObj.variety,
-        gradingCompany: cardObj.gradingCompany, grade: cardObj.grade, certificationNumber: cardObj.certificationNumber,
-        frontCardImageLink: cardObj.frontCardImageLink, backCardImageLink: cardObj.backCardImageLink,
-        sold: cardObj.sold 
-    };
     
-    return await Item.updateOne({ _id: cardId }, myObj);
+    return await Card.updateOne({ _id: cardId }, cardObj);
 }
--- routes/cards.js
+++ routes/cards.js
@@ -16,4 +16,33 @@ router.post("/", async (req, res, next) => {
     }
 });
 
+router.put("/:id", async (req, res, next) => {
+    const cardId = req.params.id;
+    const card = req.body;
+    if(cardId && card) {
+        try {
+            const updatedCard = await cardsDAO.updateCard(cardId, card);
+            res.json(updatedCard);
+        } catch(err) {
+            next(err);
+        }
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    const cardId = req.params.id;
+    if(cardId) {
+        try {
+            const card = await cardsDAO.getCard(cardId);
+            res.json(card);
+        } catch(err) {
+            next(err);
+        }
+    } else {
+        res.sendStatus(400);
+    }
+});
+
 module.exports = router;
