062bfd3d9cd0a7e579b863ad8db8eda65c6857c7
Theodore Roley <tvroley@uw.edu>
2024-05-20 21:13:42 -0700

    auth route unit tests, trying to create duplicate user sends 409 status

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const bcrypt = require('bcrypt');
+const errors = require('../middleware/errors');
 
 const userDAO = require('../daos/user');
 const middleware = require('../middleware/authenticate');
@@ -9,8 +10,8 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
 router.post("/signup", async (req, res, next) => {
-    if(req.body.hasOwnProperty('password') && req.body.password && req.body.password.trim().length !== 0 && 
-      req.body.hasOwnProperty('username') && req.body.username && req.body.username.trim().length !== 0) {
+    if(req.body.password && req.body.password.trim().length !== 0 && req.body.username && 
+        req.body.username.trim().length !== 0) {
         const textPassword = req.body.password;
         const username = req.body.username;
         const hash = await bcrypt.hash(textPassword, 2);
@@ -18,7 +19,11 @@ router.post("/signup", async (req, res, next) => {
             const collection = await userDAO.createUser({username: username, password: hash});
             res.json(collection);
         } catch(err) {
-            next(err);
+            if(err instanceof errors.DuplicateKeyError) {
+                res.status(409).send('username already used');
+            } else {
+                next(err);
+            }
         }
     } else {
       res.sendStatus(400);
@@ -26,8 +31,8 @@ router.post("/signup", async (req, res, next) => {
 });
 
 router.post("/login", async (req, res, next) => {
-    if(req.body.hasOwnProperty('password') && req.body.password.trim().length !== 0 && 
-      req.body.hasOwnProperty('username') && req.body.username && req.body.username.trim().length !== 0) {
+    if(req.body.password && req.body.password.trim().length !== 0 && req.body.username && 
+        req.body.username.trim().length !== 0) {
       const textPassword = req.body.password;
       const username = req.body.username;
       let storedUser;
@@ -64,7 +69,7 @@ router.post("/logout", async (req, res, next) => {
 router.use(middleware.isAuthenticated);
 
 router.put("/password", async (req, res, next) => {
-    if(req.body.hasOwnProperty('password') && req.body.password) {
+    if(req.body.password) {
         const textPassword = req.body.password;
         const user = req.user;
         const hashPassword = await bcrypt.hash(textPassword, 2);
