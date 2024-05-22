0fa75b87e0995e250f9d36edea66b6b8036bd100
Theodore Roley <tvroley@uw.edu>
2024-05-21 20:49:54 -0700

    handleError middleware function added to route index file

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const router = Router();
 
 const middleware = require('../middleware/authenticate');
+const { handleError } = require("../middleware/errors");
 
 router.use("/auth", require('./auth'));
 router.use(middleware.isAuthenticated);
 router.use("/cards", require('./cards'));
 router.use("/collections", require('./collections'));
+router.use(handleError);
 
 module.exports = router;
