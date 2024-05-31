0a2e62fc9909ecd3c106ba30ee5eed2bbaa31d72
Theodore Roley <tvroley@uw.edu>
2024-05-31 01:01:52 -0700

    deleted unused test-utils functions

--- test-utils.js
+++ test-utils.js
@@ -19,19 +19,3 @@ module.exports.stopDB = async () => {
 module.exports.clearDB = async () => {
   await Promise.all(models.map((model) => model.deleteMany()));
 };
-
-module.exports.findOne = async (model, query) => {
-  const result = await model.findOne(query).lean();
-  if (result) {
-    result._id = result._id.toString();
-  }
-  return result;
-};
-
-module.exports.find = async (model, query) => {
-  const results = await model.find(query).lean();
-  results.forEach((result) => {
-    result._id = result._id.toString();
-  });
-  return results;
-};
