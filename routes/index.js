348e2701850843fe6a24d735a869d7abf5eb75ad
Theodore Roley <tvroley@uw.edu>
2024-06-13 18:30:51 -0700

    added websites to cors options

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,23 @@ const router = Router();
 const middleware = require("../middleware/authenticate");
 const { handleError } = require("../middleware/errors");
 
-router.use(cors());
+const allowedOrigins = ["http://localhost:5173", "https://trading-cards-backend-production.up.railway.app/"];
+
+const corsOptions = {
+    origin: function(origin, callback){
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+router.use(cors(corsOptions));
 
 router.use("/auth", require("./auth"));
 router.use(middleware.isAuthenticated);
